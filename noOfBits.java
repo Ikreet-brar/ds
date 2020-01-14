9b01e72b7e8a6bb1aff21a02de3556ee95208102
Ikreet Brar <42814469+Ikreet-brar@users.noreply.github.com>
2020-01-15 00:07:48 +0530

    Create noOfBits.java

---
+++ noOfBits.java
@@ -0,0 +1,11 @@
+public class noOfBits {
+
+	public static void main(String[] args) {
+		int n; int bits;
+		n=65;
+		bits=(int)(Math.log(n)/Math.log(2)+1);
+		System.out.println("Number of bits are: " + bits);
+		
+	}
+
+}
